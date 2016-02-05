fec2d373d2cfd4d818f60d0baade711b6e7c037e
Marilyn Flynn <meflynn@usgs.gov>
2016-02-05 09:22:19 -0700

    add curly braces to variables; added comment about usage

--- do_stats.sh
+++ do_stats.sh
@@ -1,7 +1,8 @@
 #Runs goostats on list of files
+#Takes a list of files
 
 for datafile in "$@"
 do
-	echo $datafile
-	bash goostats $datafile stats-$datafile
+	echo ${datafile}
+	bash goostats ${datafile} stats-${datafile}
 done
